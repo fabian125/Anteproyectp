369c27eb07fccb20e55df098e372b45e23400f11
azlogin <azlogin@AzVmWinServ2016.AzVmWinServ2016.j4.internal.cloudapp.net>
2017-05-08 16:09:48 -0500

    ListaValores Y ListaValoresDetalle

---
+++ edu.uniajc.logic/src/java/edu/uniajc/anteproyecto/logic/services/ListaValorDetalleServices.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniajc.anteproyecto.logic.services;
+
+import edu.uniajc.Anteproyecto.DAO.*;
+import edu.uniajc.anteproyecto.interfaces.*;
+
+import edu.uniajc.anteproyecto.interfaces.model.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+/**
+ *
+ * @author Leon
+ */
+
+public class ListaValorDetalleServices implements IListaValoresDetalle{
+
+
+    @Override
+    public List<ListaValoresDetalle> getListaValorDetallebyID_Lista_Valor(int id_ListaValor) {
+         try {
+
+           
+                // se adquiere la conexion a base de datos desde el servidor de aplicaciones
+               // Connection dbConnection = ((DataSource) new InitialContext().lookup("jdbc/sample")).getConnection();
+                ConexionBD cn = new ConexionBD(); 
+              ListaValoresDetalleDAO dao = new ListaValoresDetalleDAO(cn.conexion());
+                	
+
+                
+                List<ListaValoresDetalle> list = dao.getListaValorDetallebyID_Lista_Valor(id_ListaValor);
+                 cn.conexion().close();
+                
+                return list;
+           
+                
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+}
