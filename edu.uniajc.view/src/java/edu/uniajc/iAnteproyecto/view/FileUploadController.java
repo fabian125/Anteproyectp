2cc3abeecb8893b45e8c093a521b516971e58c06
jarteaga <jarteaga@PC1>
2017-05-29 22:08:48 -0500

    Se crea  ListaValor

---
+++ edu.uniajc.view/src/java/edu/uniajc/iAnteproyecto/view/FileUploadController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniajc.iAnteproyecto.view;
+
+/**
+ *
+ * @author jarteaga
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.event.FileUploadEvent;
+  
+@ManagedBean(name="fileUploadController")
+public class FileUploadController {
+   private String destination="D:\\tmp\\";
+ 
+    public void upload(FileUploadEvent event) {  
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file        
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }  
+ 
+    public void copyFile(String fileName, InputStream in) {
+           try {
+              
+              
+                // write the inputStream to a FileOutputStream
+                OutputStream out = new FileOutputStream(new File(destination + fileName));
+              
+                int read = 0;
+                byte[] bytes = new byte[1024];
+              
+                while ((read = in.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+              
+                in.close();
+                out.flush();
+                out.close();
+              
+                System.out.println("New file created!");
+                } catch (IOException e) {
+                System.out.println(e.getMessage());
+                }
+    }
+}
